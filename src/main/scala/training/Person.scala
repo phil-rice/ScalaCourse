0ede75b4b20b201349e357ffad40d9b54da6d90f
Phil <phil.rice@iee.org>
2017-05-23 13:46:34 +0200

    Curried

--- src/main/scala/training/Person.scala
+++ src/main/scala/training/Person.scala
@@ -1,19 +1,30 @@
 package training
 
+import java.io.InputStream
+
+import scala.io.Source
+
+
 case class Person(id: Int, name: String, phones: List[Phone] = List())
 
 object Person {
-  def parse(raw: String, phoneMap: PhoneMap)(implicit personParser: PersonParser) = personParser(raw, phoneMap)
+  def loadFromStream(phoneMap: PhoneMap)(stream: InputStream)(implicit personParser: PersonParser): Iterator[Person] =
+    Source.fromInputStream(stream).getLines().map(personParser(phoneMap))
+
+  def parse(phoneMap: PhoneMap)(raw: String)(implicit personParser: PersonParser) = personParser(phoneMap)(raw)
 }
 
-trait PersonParser extends ((String,PhoneMap) => Person)
+trait PersonParser extends {
+  def apply(phoneMap: PhoneMap)(raw: String)(implicit personParser: PersonParser): Person
+}
 
 object PersonParser {
 
   implicit object DefaultPersonParser extends PersonParser {
-    override def apply(raw: String, phoneMap:PhoneMap): Person = raw.split(",") match {
+    override def apply(phoneMap: PhoneMap)(raw: String)(implicit personParser: PersonParser): Person = raw.split(",") match {
       case Array(id, name) => Person(id.toInt, name, phoneMap.getOrElse(id.toInt, List()))
       case _ => throw new PersonParserException(s"Cannot parse $raw")
     }
   }
+
 }
--- src/test/scala/training/PersonSpec.scala
+++ src/test/scala/training/PersonSpec.scala
@@ -3,18 +3,18 @@ package training
 
 class PersonParserSpec extends TrainingSpec with PhoneFixture {
   behavior of "DefaultPersonParser"
-  val defaultPersonParser = implicitly[PersonParser]
+  val defaultPersonParser = implicitly[PersonParser].apply(phoneMap) _
 
   it should "parse people" in {
-    defaultPersonParser("1,Phil", phoneMap) shouldBe Person(1, "Phil", List(phone1, phone2))
+    defaultPersonParser("1,Phil") shouldBe Person(1, "Phil", List(phone1, phone2))
   }
   it should "throw an error if it cannot parse the string" in {
-    intercept[PersonParserException](defaultPersonParser("some invalid text", phoneMap)).getMessage shouldBe "Cannot parse some invalid text"
+    intercept[PersonParserException](defaultPersonParser("some invalid text")).getMessage shouldBe "Cannot parse some invalid text"
   }
 }
 
 class PersonSpec extends TrainingSpec with PhoneFixture {
-  behavior of "Person.scala.parse"
+  behavior of "Person.parse"
 
   val person1 = Person(1, "Phil", List(phone1, phone2))
   val person2 = Person(2, "Bob", List())
@@ -22,8 +22,19 @@ class PersonSpec extends TrainingSpec with PhoneFixture {
 
   it should "delegate to PersonParser" in {
     implicit val phoneParser = mock[PersonParser]
-    (phoneParser.apply _) expects ("one", phoneMap) returns person1
+    (phoneParser.apply(_: PhoneMap)(_: String)(_: PersonParser)).expects(phoneMap, "one", phoneParser).returns(person1)
+    Person.parse(phoneMap)("one") shouldBe person1
+  }
 
-    Person.parse("one", phoneMap) shouldBe person1
+  behavior of "Person.loadFromStream"
+
+  it should "load people from a file using the personParser" in {
+    val stream = getClass.getClassLoader.getResourceAsStream("csvForTest.csv")
+    implicit val personParser = mock[PersonParser]
+    (personParser.apply(_: PhoneMap)(_: String)(_: PersonParser)).expects(phoneMap, "one", personParser).returns(person1)
+    (personParser.apply(_: PhoneMap)(_: String)(_: PersonParser)).expects(phoneMap, "two", personParser).returns(person2)
+    (personParser.apply(_: PhoneMap)(_: String)(_: PersonParser)).expects(phoneMap, "three", personParser).returns(person3)
+
+    Person.loadFromStream(phoneMap)(stream).toList shouldBe List(person1, person2, person3)
   }
 }
