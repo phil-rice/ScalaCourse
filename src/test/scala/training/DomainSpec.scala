bea679fd2967cba1ff00ac6779cca3d82e9a4176
Phil <phil.rice@iee.org>
2017-05-23 11:02:11 +0200

    Starting to parse the People

--- src/test/scala/training/DomainSpec.scala
+++ src/test/scala/training/DomainSpec.scala
@@ -5,6 +5,13 @@ import org.scalatest.{FlatSpec, Matchers}
 
 trait TrainingSpec extends FlatSpec with Matchers with MockFactory
 
+trait PhoneFixture {
+  val phone1 = Phone(1, "mobile", "+44 123 456 777")
+  val phone2 = Phone(1, "home", "0123 432 222")
+  val phone3 = Phone(3, "mobile", "07767 555 5555")
+
+  val phoneMap = Map(1 -> List(phone1, phone2), 3 -> List(phone3))
+}
 
 class PhoneParserSpec extends TrainingSpec {
 
@@ -18,11 +25,7 @@ class PhoneParserSpec extends TrainingSpec {
   }
 }
 
-class PhoneSpec extends TrainingSpec {
-
-  val phone1 = Phone(1, "mobile", "+44 123 456 777")
-  val phone2 = Phone(1, "home", "0123 432 222")
-  val phone3 = Phone(3, "mobile", "07767 555 5555")
+class PhoneSpec extends TrainingSpec with PhoneFixture {
 
   behavior of "Phone.parse"
 
@@ -35,7 +38,6 @@ class PhoneSpec extends TrainingSpec {
 
   behavior of "Phone.loadFromFile"
 
-
   it should "Load from stream, using the parser" in {
     val stream = getClass.getClassLoader.getResourceAsStream("csvForTest.csv")
     implicit val phoneParser = stub[PhoneParser]
@@ -45,13 +47,28 @@ class PhoneSpec extends TrainingSpec {
 
     Phone.loadFromStream(stream).toList shouldBe List(phone1, phone2, phone3)
   }
-
 }
 
-class PersonSpec extends TrainingSpec {
-  val phil = Person(1, "Phil")
-  it should "have an id field and a name field" in { //we wouldn’t normally test getters
-    phil.id shouldBe 1
-    phil.name shouldBe "Phil"
+class PersonParserSpec extends TrainingSpec with PhoneFixture {
+  behavior of "DefaultPersonParser"
+  val defaultPersonParser = implicitly[PersonParser]
+
+  it should "parse people" in {
+    defaultPersonParser("1,Phil", phoneMap) shouldBe Person(1, "Phil", List(phone1, phone2))
+  }
+}
+
+class PersonSpec extends TrainingSpec with PhoneFixture {
+  behavior of "Person.parse"
+
+    val person1 = Person(1, "Phil", List(phone1, phone2))
+    val person2 = Person(2, "Bob", List())
+    val person3 = Person(3, "Jill", List(phone3))
+
+  it should "delegate to PersonParser" in {
+    implicit val phoneParser = stub[PersonParser]
+    (phoneParser.apply _) when("one", phoneMap) returns person1
+
+    Person.parse("one", phoneMap) shouldBe person1
   }
 }
